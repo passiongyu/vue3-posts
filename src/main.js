0ba63885f4df394e59cb47cd0b73d737fb1b9f45
passiongyu <passiongyu7606@gmail.com>
2025-11-12 23:36:25 +0900

    라우팅설정

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 
+//모든 자식 컴포넌트에 router, route같은 객체를 사용할 수 있음.
 createApp(App).use(router).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <h2>About vue</h2>
+    <p>{{ $route.path }}</p>
+    <button class="btn btn-primary" @click="$router.push('/')">
+      HOME으로 이동
+    </button>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log('route.path: ', route.path)
+</script>
 
 <style lang="scss" scoped></style>
